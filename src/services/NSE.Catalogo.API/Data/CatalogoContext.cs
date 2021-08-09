36d32f7bc971b0ed926f5095ca870ceb2049ab25
David <luizdavid.93@hotmail.com>
2021-08-09 06:58:54 -0300

    Acesso a dados API Catálogo

---
+++ src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext, IUnityOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) :base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties()
+            .Where(p => p.ClrType == typeof(string)))) property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+    }
+}
