627bdf75685a0e119a05167241429db9a464e972
David <luizdavid.93@hotmail.com>
2022-01-19 15:36:39 -0300

    API de Clientes - Command e Command Handler

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation.Results;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command: Message
+    {
+        public DateTime TimeStamp { get; set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            // validar cmd
+
+            // persistir na base
+        }
+    }
+}
