03e043b221f8396357a0161acdb01033c2667b94
David Oliveira <luiz.oliveira@siemens-energy.com>
2022-05-06 09:03:04 -0300

    Readme update

--- src/services/NSE.Pedido.API/Startup.cs
+++ src/services/NSE.Pedido.API/Startup.cs
@@ -25,27 +25,13 @@ namespace NSE.Pedido.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            
         }
     }
 }
