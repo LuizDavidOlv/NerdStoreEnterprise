e6e75c4b6949c1cb260e2ac63b0024f396c5b246
David Oliveira <luiz.oliveira@siemens-energy.com>
2022-06-05 18:40:13 -0300

    Configuring MessageBus na API de Carrinho e Catalago

--- src/services/NSE.Pedidos.Infra/Data/Repository/VoucherRepository.cs
+++ src/services/NSE.Pedidos.Infra/Data/Repository/VoucherRepository.cs
@@ -12,11 +12,12 @@ namespace NSE.Pedidos.Infra.Data.Repository
     {
 
         private readonly PedidosContext _context;
-        public IUnitOfWork UnitOfWork => _context;
         public VoucherRepository(PedidosContext context)
         {
             _context = context;
         }
+
+        public IUnitOfWork UnitOfWork => _context;
 
         public async Task<Voucher> ObterVoucherPorCodigo(string codigo)
         {
