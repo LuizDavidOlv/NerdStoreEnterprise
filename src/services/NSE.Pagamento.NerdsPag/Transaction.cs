7198c2f5757286260a0eec73d98cc0eb9c1c3c16
David Oliveira <luiz.oliveira@siemens-energy.com>
2022-07-06 20:32:14 -0300

    Reconfiguring  transactions to not be accepted everytime

--- src/services/NSE.Pagamento.NerdsPag/Transaction.cs
+++ src/services/NSE.Pagamento.NerdsPag/Transaction.cs
@@ -87,7 +87,7 @@ namespace NSE.Pagamento.NerdsPag
                 TransactionDate = DateTime.Now,
                 Cost = 0,
                 Amount = 0,
-                Status = TransactionStatus.Authorized, //Refused Lembrar de mudar depois dos testes
+                Status = TransactionStatus.Refused,
                 Tid = "",
                 Nsu = ""
             };
