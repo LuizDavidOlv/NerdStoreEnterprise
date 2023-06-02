4e44a00fb916950328704e7339c79f486472410d
David Oliveira <Luiz.Oliveira2@Dell.com>
2023-06-02 17:08:28 -0300

    Headders added to kafka producer and consumers

--- src/building-blocks/NSE.MessageBus/KafkaBus.cs
+++ src/building-blocks/NSE.MessageBus/KafkaBus.cs
@@ -1,8 +1,11 @@
 ﻿using Confluent.Kafka;
 using Confluent.Kafka.Admin;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Core.Messages.Integration;
+using NSE.MessageBus.Serializador;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,11 +25,98 @@ namespace NSE.MessageBus
         {
             var config = new ProducerConfig
             {
+                //This option is used to send messages in order
+                EnableIdempotence = true,
+                MaxInFlight = 1,
+                MessageSendMaxRetries = 2,
+                Acks = Acks.All,
+                //
+                //Acks = Acks.Leader,
                 BootstrapServers = _bootstrapserver,
             };
 
-            var payload = System.Text.Json.JsonSerializer.Serialize(message);
+            //var payload = System.Text.Json.JsonSerializer.Serialize(message);
 
+            CreateTopicIfNecessary(topic);
+
+            var producer = new ProducerBuilder<string, T>(config)
+                .SetValueSerializer(new SerializerNSE<T>())
+                .Build();
+
+            var headers = new Headers();
+            headers.Add("application", Encoding.UTF8.GetBytes("payment"));
+            headers.Add("application", Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+
+
+            var result = await producer.ProduceAsync(topic, new Message<string, T>
+            {
+                Key = Guid.NewGuid().ToString(),
+                Value = message,
+                Headers = headers,
+            });
+
+            //This option is used to commit manually
+            producer.CommitTransaction();
+            //This option is used to abort manually
+                //producer.AbortTransaction();
+
+            await Task.CompletedTask;
+        }
+
+        public async Task ConsumerAsync<T>(
+            string topic,
+            Func<T, Task> onMessage,
+            CancellationToken cancellation) where T : IntegrationEvent
+        {
+            _ = Task.Factory.StartNew(async () =>
+            {
+                var config = new ConsumerConfig
+                {
+                    //This option is used to read all messages from the beginning
+                        //AutoOffsetReset = AutoOffsetReset.Earliest,
+                    //This option is used to read only new messages
+                        //AutoOffsetReset = AutoOffsetReset.Latest,
+                    GroupId = "grupo-curso",
+                    BootstrapServers = _bootstrapserver,
+                    EnableAutoCommit = false,
+                    EnablePartitionEof = true,
+                    //This option is used to commit manually
+                    EnableAutoOffsetStore = false,
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                };
+
+                using var consumer = new ConsumerBuilder<string, T>(config)
+                     .SetValueDeserializer(new DeserializerNSE<T>())
+                     .Build();
+
+                consumer.Subscribe(topic);
+
+                while (!cancellation.IsCancellationRequested)
+                {
+                    var result = consumer.Consume();
+
+                    if(result.IsPartitionEOF)
+                    {
+                        continue;
+                    }
+
+                    var headers = result.Message.Headers
+                    .ToDictionary(p => p.Key, p => Encoding.UTF8.GetString(p.GetValueBytes()));
+
+                    var application = headers["application"];
+                    var transactionId = headers["transactionId"];
+
+                    //var message = System.Text.Json.JsonSerializer.Deserialize<T>(result.Message.Value);
+                    await onMessage(result.Message.Value);
+                    consumer.Commit();
+                }
+            }, cancellation, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+            await Task.CompletedTask; 
+        }
+
+        private async void CreateTopicIfNecessary(string topic)
+        {
             var adminClientConfig = new AdminClientConfig
             {
                 BootstrapServers = _bootstrapserver
@@ -45,52 +135,6 @@ namespace NSE.MessageBus
                     Console.WriteLine($"An error occured creating topic {e.Results[0].Topic}: {e.Results[0].Error.Reason}");
                 }
             }
-
-            var producer = new ProducerBuilder<string, string>(config).Build();
-
-            var result = await producer.ProduceAsync(topic, new Message<string, string>
-            {
-                Key = Guid.NewGuid().ToString(),
-                Value = payload
-            });
-
-            await Task.CompletedTask;
-        }
-
-        public async Task ConsumerAsync<T>(
-            string topic,
-            Func<T, Task> onMessage,
-            CancellationToken cancellation) where T : IntegrationEvent
-        {
-            _ = Task.Factory.StartNew(async () =>
-            {
-                var config = new ConsumerConfig
-                {
-                    GroupId = "grupo-curso",
-                    BootstrapServers = _bootstrapserver,
-                    EnableAutoCommit = false,
-                    EnablePartitionEof = true,
-                };
-
-                using var consumer = new ConsumerBuilder<string, string>(config).Build();
-                consumer.Subscribe(topic);
-
-                while (!cancellation.IsCancellationRequested)
-                {
-                    var result = consumer.Consume();
-
-                    if(result.IsPartitionEOF)
-                    {
-                        continue;
-                    }
-
-                    var message = System.Text.Json.JsonSerializer.Deserialize<T>(result.Message.Value);
-                    await onMessage(message);
-                    consumer.Commit();
-                }
-            }, cancellation, TaskCreationOptions.LongRunning, TaskScheduler.Default);
-
-            await Task.CompletedTask; 
         }
 
         public void Dispose()
